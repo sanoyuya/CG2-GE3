0a676b1028a6f5d1f233052447f3d0b59a1ef81b
sano_yuya <k021g1256@g.neec.ac.jp>
2022-11-16 02:24:17 +0900

    XY連番対応

--- DirectXGame/Sprite.h
+++ DirectXGame/Sprite.h
@@ -97,7 +97,20 @@ public:
 	/// <param name="flipY">Y反転</param>
 	void DrawAnimationSpriteY(TextureData& textureData, myMath::Vector2 position, uint16_t sheetsNum, uint16_t& nowNum, myMath::Vector4 color = { 1.0f,1.0f ,1.0f ,1.0f }, myMath::Vector2 scale = { 1.0f,1.0f }, float rotation = 0.0f, myMath::Vector2 anchorpoint = { 0.5f,0.5f }, bool flipX = false, bool flipY = false);
 
-
+	/// <summary>
+	/// 縦横連番スプライト描画
+	/// </summary>
+	/// <param name="textureData">テクスチャデータ</param>
+	/// <param name="position">座標</param>
+	/// <param name="sheetsNumX">横の枚数</param>
+	/// <param name="sheetsNumY">縦の枚数</param>
+	/// <param name="nowNum">何枚目か</param>
+	/// <param name="color">色</param>
+	/// <param name="scale">大きさ</param>
+	/// <param name="rotation">回転</param>
+	/// <param name="anchorpoint">中心点</param>
+	/// <param name="flipX">X反転</param>
+	/// <param name="flipY">Y反転</param>
 	void DrawAnimationSpriteXY(TextureData& textureData, myMath::Vector2 position, uint16_t sheetsNumX, uint16_t sheetsNumY, uint16_t& nowNum, myMath::Vector4 color = { 1.0f,1.0f ,1.0f ,1.0f }, myMath::Vector2 scale = { 1.0f,1.0f }, float rotation = 0.0f, myMath::Vector2 anchorpoint = { 0.5f,0.5f }, bool flipX = false, bool flipY = false);
 
 	/// <summary>
